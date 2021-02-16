1d9d912e7ecc13fb4d62192ad4e87216b79892ed
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 16:37:35 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LesConditionsExo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ///Créer 2 variables : login et password, et leur attribuer une valeur. 
+            ///À l’aide de la console, inviter l’utilisateur à renseigner un identifiant et un mot de passe. 
+            ///Dans la console, selon les informations renseignées, afficher un des messages suivants : 
+            ///« Mauvais identifiant ou mauvais mot de passe. » « Bienvenue à la Manu. » 
+            ///Utiliser une condition switch.
+            
+
+
+        }
+    }
+}
